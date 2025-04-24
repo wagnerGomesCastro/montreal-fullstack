3a9a796af5c863700131463c1b0b2503d4fdf903
Wagner Castro <wagner_gcastro@hotmail.com>
2025-04-24 15:20:36 -0300

    #09 (backend) - Added Table list users fake
    - added base layout

---
+++ packages/frontend/src/layouts/BaseLayout.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="page-container">
+    <div id="main-container">
+      <RouterView />
+    </div>
+  </div>
+</template>
--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -4,7 +4,19 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Template components
+import BaseBlock from '@/components/BaseBlock/index.vue'
+import BasePageHeading from '@/components/BasePageHeading/index.vue'
+
+// Bootstrap framework
+import * as bootstrap from 'bootstrap'
+window.bootstrap = bootstrap
+
 const app = createApp(App)
+
+// Register global components
+app.component('BaseBlock', BaseBlock)
+app.component('BasePageHeading', BasePageHeading)
 
 app.use(createPinia())
 app.use(router)
